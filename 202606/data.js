ce4f2ad89bab46ba8653e4a028f014994b43904e
dxl <286613623@qq.com>
2026-06-25 17:22:13 +0800

    feat: 汇总表显示完整刀型ID，视频/轴图数据移入 data.js

---
+++ 202606/data.js
@@ -0,0 +1,636 @@
+var KNIFE_DATA = {
+  "knife_types": [
+    {
+      "id": "B1-A",
+      "zone": "B",
+      "description": "611 水剑 但丁 水兔 水电 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B1-B",
+      "zone": "B",
+      "description": "狼 格蕾丝 战病 白望 美杜莎 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B1-C",
+      "zone": "B",
+      "description": "白猫 原花 露娜 艾姬多娜 兰法 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B2-A",
+      "zone": "B",
+      "description": "611 水剑 但丁 水兔 水电 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B2-B",
+      "zone": "B",
+      "description": "狼 格蕾丝 战病 白望 美杜莎 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B2-C",
+      "zone": "B",
+      "description": "白猫 原花 露娜 艾姬多娜 兰法 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B3-A",
+      "zone": "B",
+      "description": "611 水剑 但丁 水兔 水电 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B4-A",
+      "zone": "B",
+      "description": "狼 格蕾丝 战病 白望 美杜莎 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "B5-A",
+      "zone": "B",
+      "description": "白猫 原花 露娜 艾姬多娜 兰法 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C1-A",
+      "zone": "C",
+      "description": "江月 小美 军病 鼠鼠 原克 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C1-B",
+      "zone": "C",
+      "description": "风剑 若菜 生菜 栞 菲欧 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C1-C",
+      "zone": "C",
+      "description": "黑骑 611 星猫 克 笑哈哈 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C2-A",
+      "zone": "C",
+      "description": "513 吉塔 白望 小小甜心 晶 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C2-B",
+      "zone": "C",
+      "description": "风剑 若菜 生菜 栞 菲欧 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C2-C",
+      "zone": "C",
+      "description": "黑骑 611 星猫 克 笑哈哈 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C3-A",
+      "zone": "C",
+      "description": "魔姬 龙妈 白猫 原花 els 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C3-B",
+      "zone": "C",
+      "description": "黑骑 611 星猫 克 笑哈哈 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C4-A",
+      "zone": "C",
+      "description": "513 吉塔 白望 小小甜心 晶 返90s",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C4-B",
+      "zone": "C",
+      "description": "魔姬 龙妈 白猫 原花 els 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C4-C",
+      "zone": "C",
+      "description": "黑骑 611 星猫 克 笑哈哈 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C5-A",
+      "zone": "C",
+      "description": "魔姬 龙妈 白猫 原花 els 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "C5-B",
+      "zone": "C",
+      "description": "黑骑 611 星猫 克 笑哈哈 返57+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "D1-A",
+      "zone": "D",
+      "description": "紫罗兰 水龙 原花 兰法 els 0.5押30120w",
+      "video_url": "https://www.bilibili.com/video/BV1uEjS6wEGd",
+      "video_source": "浮城墟烟",
+      "axis_image": "D1_轴图.png"
+    },
+    {
+      "id": "D1-B",
+      "zone": "D",
+      "description": "紫罗兰 水龙 原花 兰法 els 手动 30000w+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "D2-A",
+      "zone": "D",
+      "description": "剑圣 若菜 蝶妈 栞 菲欧 0.5押30470w",
+      "video_url": "https://www.bilibili.com/video/BV1n2jy6qE4c",
+      "video_source": "浮城墟烟",
+      "axis_image": "D2_轴图.png"
+    },
+    {
+      "id": "D2-B",
+      "zone": "D",
+      "description": "剑圣 若菜 蝶妈 栞 菲欧 手动 31000w+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "D3-A",
+      "zone": "D",
+      "description": "情姐 斑比 火机 圣莱 色鹿 0.5押29680w",
+      "video_url": "https://www.bilibili.com/video/BV1LPjS6rE25",
+      "video_source": "浮城墟烟",
+      "axis_image": "D3_轴图.png"
+    },
+    {
+      "id": "D3-B",
+      "zone": "D",
+      "description": "手动 31000w+",
+      "video_url": "",
+      "video_source": "",
+      "axis_image": ""
+    },
+    {
+      "id": "D4-A",
+      "zone": "D",
+      "description": "水田 苑 水晶 炼金望 sms 0押35000w",
+      "video_url": "https://www.bilibili.com/video/BV1Stjy6SE3c",
+      "video_source": "浮城墟烟",
+      "axis_image": "D4_轴图.png"
+    },
+    {
+      "id": "D5-A",
+      "zone": "D",
+      "description": "水切 吹雪 情姐 凤凰 涅妃 0押28416w",
+      "video_url": "https://www.bilibili.com/video/BV1mhjy6mEUj",
+      "video_source": "浮城墟烟",
+      "axis_image": "D5_轴图.png"
+    }
+  ],
+  "users": [
+    {
+      "rank": 1,
+      "name": "水月陵",
+      "level": 331,
+      "knives": [
+        "B5-A",
+        "B4-A",
+        "B3-A",
+        "D1-B",
+        "D3-B",
+        "D2-B"
+      ]
+    },
+    {
+      "rank": 2,
+      "name": "炫斩星空",
+      "level": 329,
+      "knives": [
+        "B5-A",
+        "B4-A",
+        "B3-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 3,
+      "name": "歪歪 双场老6已建群",
+      "level": 328,
+      "knives": [
+        "B5-A",
+        "B4-A",
+        "B3-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 4,
+      "name": "布丁可爱捏",
+      "level": 326,
+      "knives": [
+        "B5-A",
+        "B4-A",
+        "B3-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 5,
+      "name": "聪明猫猫贴狼宝",
+      "level": 326,
+      "knives": [
+        "B1-C",
+        "B2-B",
+        "B3-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 6,
+      "name": "伯言之颜 占3",
+      "level": 326,
+      "knives": [
+        "B2-C",
+        "B1-B",
+        "B3-A",
+        "D1-B",
+        "D3-B",
+        "D2-B"
+      ]
+    },
+    {
+      "rank": 7,
+      "name": "田玉树",
+      "level": 325,
+      "knives": [
+        "B1-C",
+        "B4-A",
+        "B2-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 8,
+      "name": "晴菏",
+      "level": 324,
+      "knives": [
+        "B2-C",
+        "B4-A",
+        "B1-A",
+        "D2-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 9,
+      "name": "小触",
+      "level": 318,
+      "knives": [
+        "B5-A",
+        "B1-B",
+        "B2-A",
+        "D1-B",
+        "D3-B",
+        "D2-B"
+      ]
+    },
+    {
+      "rank": 10,
+      "name": "优衣的小窝挖",
+      "level": 317,
+      "knives": [
+        "B5-A",
+        "B2-B",
+        "B1-A",
+        "D1-B",
+        "D3-B",
+        "D2-B"
+      ]
+    },
+    {
+      "rank": 11,
+      "name": "你一定在逗我",
+      "level": 315,
+      "knives": [
+        "C1-B",
+        "C1-A",
+        "C4-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 12,
+      "name": "星空可睹丶",
+      "level": 314,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C2-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 13,
+      "name": "莉姆露酱",
+      "level": 314,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C4-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 14,
+      "name": "波拉利斯",
+      "level": 313,
+      "knives": [
+        "C1-B",
+        "C1-A",
+        "C2-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 15,
+      "name": "赏膳罚饿使",
+      "level": 312,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C2-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 16,
+      "name": "悲催",
+      "level": 312,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C4-A",
+        "D2-A",
+        "D1-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 17,
+      "name": "夙夙：棣！子！",
+      "level": 311,
+      "knives": [
+        "C1-B",
+        "C1-A",
+        "C2-A",
+        "D1-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 18,
+      "name": "雾岛薰香",
+      "level": 311,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C2-A",
+        "D1-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 19,
+      "name": "流云易水",
+      "level": 310,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C4-A",
+        "D1-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 20,
+      "name": "rainyNight",
+      "level": 310,
+      "knives": [
+        "C2-B",
+        "C1-A",
+        "C2-A",
+        "D1-A",
+        "D4-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 21,
+      "name": "为什么这样子",
+      "level": 314,
+      "knives": [
+        "C1-C",
+        "C5-A",
+        "C4-A",
+        "C5-B",
+        "C3-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 22,
+      "name": "潘达",
+      "level": 313,
+      "knives": [
+        "C1-C",
+        "C5-A",
+        "C4-A",
+        "C5-B",
+        "C3-A",
+        "D5-A"
+      ]
+    },
+    {
+      "rank": 23,
+      "name": "西红柿煮鸡蛋",
+      "level": 312,
+      "knives": [
+        "C1-C",
+        "C5-A",
+        "C4-A",
+        "C5-B",
+        "C3-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 24,
+      "name": "一刀羞萝",
+      "level": 312,
+      "knives": [
+        "C3-B",
+        "C4-B",
+        "C4-A",
+        "C5-B",
+        "C3-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 25,
+      "name": "fou",
+      "level": 309,
+      "knives": [
+        "C4-C",
+        "C5-A",
+        "C4-A",
+        "C5-B",
+        "C3-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 26,
+      "name": "雾雨魔理哀",
+      "level": 307,
+      "knives": [
+        "C2-C",
+        "C3-A",
+        "C4-A",
+        "C3-B",
+        "C5-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 27,
+      "name": "东方君",
+      "level": 307,
+      "knives": [
+        "C2-C",
+        "C3-A",
+        "C4-A",
+        "C3-B",
+        "C5-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 28,
+      "name": "烟花",
+      "level": 305,
+      "knives": [
+        "C5-B",
+        "C3-A",
+        "C4-A",
+        "C3-B",
+        "C5-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 29,
+      "name": "无声铃鹿",
+      "level": 299,
+      "knives": [
+        "C2-C",
+        "C3-A",
+        "C4-A",
+        "C3-B",
+        "C5-A",
+        "D3-A"
+      ]
+    },
+    {
+      "rank": 30,
+      "name": "碎茶",
+      "level": 284,
+      "knives": [
+        "C5-B",
+        "C3-A",
+        "C4-A",
+        "C3-B",
+        "C5-A",
+        "D3-A"
+      ]
+    }
+  ]
+};
